6d03df88d119b5080b91d404700ec4f5d7be12c0
vladimir713 <vladimir713@mail.ru>
2022-10-24 00:06:03 +0300

    В процессе написания контроллеров

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/MyRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/MyRestController.java
@@ -1,13 +1,37 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.util.List;
 
 @RestController
 public class MyRestController {
 
-    @Autowired
     private UserService userService;
+
+    @Autowired
+    public MyRestController(UserService userService) {
+
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> showAllUsers() {
+        return userService.index();
+    }
+
+    @GetMapping("/users/{id}")
+    public User findById(@PathVariable("id") int id) {
+        return userService.show(id);
+    }
+
+    @PostMapping("/users")
+        public User addNewUser(@RequestBody User user) {
+        userService.save(user);
+        return user;
+    }
 
 }
